2a85d91e5c4fd26d73a9d3656bf369208f6f0c36
ptlis <ptlis@ptlis.net>
2015-01-19 20:47:50 +0000

    Add: Flag implementation, tests.

---
+++ src/Flag.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * PHP Version 5.3
+ *
+ * @copyright (c) 2015 brian ridley
+ * @author brian ridley <ptlis@ptlis.net>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+namespace ptlis\ShellCommand;
+
+use ptlis\ShellCommand\Interfaces\ArgumentInterface;
+
+/**
+ * Basic argument implementation, accepts a flag & (optional) value.
+ */
+class Flag implements ArgumentInterface
+{
+    /**
+     * @var string The flag to pass.
+     */
+    private $flag;
+
+    /**
+     * @var string (optional) value to pass along with flag.
+     */
+    private $value;
+
+
+    /**
+     * Constructor.
+     *
+     * @param string $flag
+     * @param string $value
+     */
+    public function __construct($flag, $value = '')
+    {
+        $this->flag = $flag;
+        $this->value = $value;
+    }
+
+    /**
+     * Returns an argument to pass to a command.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        $string = '-' . $this->flag;
+
+        if (strlen($this->value)) {
+            $string .= ' ' . $this->value;
+        }
+
+        return $string;
+    }
+}
---
+++ tests/Arguments/FlagTest.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * PHP Version 5.3
+ *
+ * @copyright (c) 2015 brian ridley
+ * @author brian ridley <ptlis@ptlis.net>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+namespace ptlis\ShellCommand\Arguments;
+
+use ptlis\ShellCommand\Flag;
+
+class FlagTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFlagOnly()
+    {
+        $flag = new Flag('v');
+
+        $this->assertEquals('-v', $flag->__toString());
+    }
+
+    public function testFlagAndValue()
+    {
+        $flag = new Flag('c', '/etc/foo.ini');
+
+        $this->assertEquals('-c /etc/foo.ini', $flag->__toString());
+    }
+}
