82400f8b64e1f0d4261422d321490f687aff1d61
ptlis <ptlis@ptlis.net>
2015-01-20 13:27:33 +0000

    Add: Parameter implementation, tests.

---
+++ src/Parameter.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * PHP Version 5.3
+ *
+ * @copyright (c) 2015 brian ridley
+ * @author brian ridley <ptlis@ptlis.net>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+namespace ptlis\ShellCommand;
+
+use ptlis\ShellCommand\Interfaces\ArgumentInterface;
+
+/**
+ * Parameter implementation, stores a parameter to pass.
+ */
+class Parameter implements ArgumentInterface
+{
+    /**
+     * Constructor.
+     *
+     * @param string $parameter
+     */
+    public function __construct($parameter)
+    {
+        $this->parameter = $parameter;
+    }
+
+    /**
+     * Returns an argument to pass to a command.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->parameter;
+    }
+}
---
+++ tests/Arguments/ParameterTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * PHP Version 5.3
+ *
+ * @copyright (c) 2015 brian ridley
+ * @author brian ridley <ptlis@ptlis.net>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+namespace ptlis\ShellCommand\Arguments;
+
+use ptlis\ShellCommand\Parameter;
+
+class ParameterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFlagOnly()
+    {
+        $parameter = new Parameter('the_file');
+
+        $this->assertEquals('the_file', $parameter->__toString());
+    }
+}
