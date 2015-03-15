64e6ca7246a6431879067538426a8c7968732db6
ptlis <ptlis@ptlis.net>
2015-03-15 19:30:43 +0000

    Fix: Make test for sigterm more robust.

--- tests/RunningProcess/UnixRunningProcessSigTermIgnoredTest.php
+++ tests/RunningProcess/UnixRunningProcessSigTermIgnoredTest.php
@@ -10,24 +10,39 @@
 
 namespace ptlis\ShellCommand\Test\RunningProcess;
 
+use Psr\Log\LogLevel;
+use ptlis\ShellCommand\Logger\SignalSentLogger;
+use ptlis\ShellCommand\Test\Logger\MockPsrLogger;
+
 class UnixRunningProcessSigTermIgnoredTest extends \PHPUnit_Framework_TestCase
 {
     public function testRunProcess()
     {
         $command = './tests/data/sleep_binary';
 
-        $process = new UnixRunningProcessBlockSigTerm($command, getcwd());
+        $logger = new MockPsrLogger();
+
+        $process = new UnixRunningProcessBlockSigTerm(
+            $command,
+            getcwd(),
+            -1,
+            1000,
+            new SignalSentLogger($logger)
+        );
 
         $process->stop();
 
         $this->assertEquals(
-            false,
-            $process->isRunning()
-        );
-
-        $this->assertEquals(
-            './tests/data/sleep_binary',
-            $process->getCommand()
+            array(
+                array(
+                    'level' => LogLevel::DEBUG,
+                    'message' => 'Signal sent',
+                    'context' => array(
+                        'signal' => 9
+                    )
+                )
+            ),
+            $logger->getLogs()
         );
     }
 }
